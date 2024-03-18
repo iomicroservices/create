0658ee7767c8e5eed619c5f7bbdfc53b6ec39618
Khalid Usman <kusman758@gmail.com>
2024-03-18 16:07:24 +0800

    Added utilities and static datas

--- types/blog.d.ts
+++ types/blog.d.ts
@@ -2,6 +2,7 @@ export { Blog };
 
 declare global {
   interface Blog {
+    id: number
     title: string
     description: string
     date_published: string
---
+++ utils/data.ts
@@ -0,0 +1,10 @@
+export const stages: Blog[] = [
+  {
+    id: 1,
+    title: 'Test',
+    description: 'Lorem ispum',
+    date_published: '2022-01-01',
+    image: '',
+    slug_url: 'test-url'
+  },
+]
