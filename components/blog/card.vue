5ad427098c2bc859b60e4cd26822cc3d6f74ea36
Khalid Usman <kusman758@gmail.com>
2024-03-20 00:10:21 +0800

    Added blog component and sample datas

---
+++ components/blog/card.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+interface Props {
+  blogs: Blog[]
+}
+
+withDefaults(defineProps<Props>(), {
+  blogs: () => ([])
+})
+
+</script>
+<template>
+  <article class="card card-hover-shadow border p-3 mb-4" v-for="blog, index in blogs" :key="index">
+    <NuxtLink :to="blog.slug_url">
+        <div class="row">
+            <div class="col-md-4">
+                <img
+                class="img-fluid card-img"
+                width="300"
+                :src="blog.image"
+                />
+            </div>
+            <div class="col-md-8">
+                <div class="card-body d-flex flex-column h-100 ps-0 pe-3">
+                    <div><p v-for="tag, index in blog.tags" :key="index" class="badge text-bg-dark mb-3 ms-1">{{ tag.name }}</p></div>
+                    <h5 class="card-title mb-3 mb-md-0">{{ blog.title }} </h5>
+                    <p> {{ blog.description.slice(0, 100) }}...</p>
+
+                    <div class="d-sm-flex justify-content-between align-items-center mt-auto">
+                        <p class="mb-2 heading-color fw-semibold">
+                            <p> {{ blog.date_published }}</p>
+                        </p>
+                        <a class="icon-link icon-link-hover stretched-link" href="#">Read more<i class="bi bi-arrow-right"></i> </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </NuxtLink>
+  </article>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
--- utils/data.ts
+++ utils/data.ts
@@ -4,14 +4,136 @@ export const blogs: Blog[] = [
     title: 'How To Properly Fetch Nuxt Content Data and Render It in Nuxt Pages',
     description: 'Here we will learn How To Properly Fetch Nuxt Content Data and Render It in Nuxt Pages',
     date_published: '2022-01-01',
-    image: '',
+    image: 'https://picsum.photos/200',
     slug_url: 'how-to-properly-fetch-nuxt-content-data-and-render-it-in-nuxt-pages',
     tags: [
       { name: 'test' }
     ],
     isPublished: true
   },
-]
+  {
+    id: 2,
+    title: 'Optimizing Your Website for Speed: Top Strategies to Boost Performance',
+    description: 'Discover effective strategies to optimize your website for speed and enhance user experience. From image optimization to code minification, learn how to achieve lightning-fast loading times.',
+    date_published: '2022-01-02',
+    image: 'https://picsum.photos/200',
+    slug_url: 'optimizing-your-website-for-speed',
+    tags: [
+      { name: 'web development' },
+      { name: 'performance' }
+    ],
+    isPublished: true
+  },
+  {
+    id: 3,
+    title: 'Mastering Responsive Design: Essential Tips and Best Practices',
+    description: 'Learn the key principles and best practices for creating responsive web designs that adapt seamlessly to various devices and screen sizes. Enhance user experience with these expert tips.',
+    date_published: '2022-01-03',
+    image: 'https://picsum.photos/200',
+    slug_url: 'mastering-responsive-design',
+    tags: [
+      { name: 'web design' },
+      { name: 'responsive' }
+    ],
+    isPublished: true
+  },
+  {
+    id: 4,
+    title: 'The Power of CSS Grid: Revolutionizing Web Layouts',
+    description: 'Unlock the full potential of CSS Grid and revolutionize your web layouts. Explore advanced techniques and practical examples to create complex yet flexible designs with ease.',
+    date_published: '2022-01-04',
+    image: 'https://picsum.photos/200',
+    slug_url: 'the-power-of-css-grid',
+    tags: [
+      { name: 'web design' },
+      { name: 'css' },
+      { name: 'layout' }
+    ],
+    isPublished: true
+  },
+  {
+    id: 5,
+    title: 'Building Scalable APIs with Node.js and Express: Best Practices',
+    description: 'Learn how to build scalable and robust APIs using Node.js and Express. From middleware usage to error handling, discover best practices to ensure your APIs are efficient and reliable.',
+    date_published: '2022-01-05',
+    image: 'https://picsum.photos/200',
+    slug_url: 'building-scalable-apis-with-nodejs-and-express',
+    tags: [
+      { name: 'node.js' },
+      { name: 'express' },
+      { name: 'api' }
+    ],
+    isPublished: true
+  },
+  {
+    id: 6,
+    title: 'Effective UX Design: Improving User Engagement and Satisfaction',
+    description: 'Explore effective UX design strategies to enhance user engagement and satisfaction. From usability testing to user-centered design principles, optimize your digital products for success.',
+    date_published: '2022-01-06',
+    image: 'https://picsum.photos/200',
+    slug_url: 'effective-ux-design',
+    tags: [
+      { name: 'ux design' },
+      { name: 'user experience' }
+    ],
+    isPublished: true
+  },
+  {
+    id: 7,
+    title: 'Creating Stunning Visual Effects with CSS Animations: Tips and Tricks',
+    description: 'Master the art of CSS animations to create stunning visual effects on your website. Learn advanced techniques, keyframe animation, and CSS animation libraries to make your designs stand out.',
+    date_published: '2022-01-07',
+    image: 'https://picsum.photos/200',
+    slug_url: 'creating-stunning-visual-effects-with-css-animations',
+    tags: [
+      { name: 'web design' },
+      { name: 'css' },
+      { name: 'animations' }
+    ],
+    isPublished: true
+  },
+  {
+    id: 8,
+    title: 'Introduction to Progressive Web Apps: Building Offline-First Web Experiences',
+    description: 'Discover the fundamentals of Progressive Web Apps (PWAs) and learn how to build web applications that offer native-like experiences. From service workers to app manifest, dive into PWA development.',
+    date_published: '2022-01-08',
+    image: 'https://picsum.photos/200',
+    slug_url: 'introduction-to-progressive-web-apps',
+    tags: [
+      { name: 'web development' },
+      { name: 'progressive web apps' },
+      { name: 'pwa' }
+    ],
+    isPublished: true
+  },
+  {
+    id: 9,
+    title: 'Data Visualization Techniques: Presenting Complex Data Simply',
+    description: 'Learn effective data visualization techniques to present complex data in a simple and understandable manner. From charts and graphs to interactive dashboards, unlock the power of visual storytelling.',
+    date_published: '2022-01-09',
+    image: 'https://picsum.photos/200',
+    slug_url: 'data-visualization-techniques',
+    tags: [
+      { name: 'data visualization' },
+      { name: 'visualization' },
+      { name: 'charts' }
+    ],
+    isPublished: true
+  },
+  {
+    id: 10,
+    title: 'Securing Your Web Applications: Best Practices for Web Security',
+    description: 'Discover essential best practices for securing your web applications against common security threats. From HTTPS implementation to input validation, ensure your applications are protected.',
+    date_published: '2022-01-10',
+    image: 'https://picsum.photos/200',
+    slug_url: 'securing-your-web-applications',
+    tags: [
+      { name: 'web development' },
+      { name: 'security' }
+    ],
+    isPublished: true
+  },
+];
 
 
 export const pages: Page[] = [
